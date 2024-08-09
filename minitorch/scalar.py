f07371858792410be9f3a7fd34358d4a56ede0fd
ysimin <ysimin@infervision.com>
2024-08-09 11:36:19 +0800

    [minitorch]task1_3

--- minitorch/scalar.py
+++ minitorch/scalar.py
@@ -182,7 +182,8 @@ class Scalar:
         assert h is not None
         assert h.last_fn is not None
         assert h.ctx is not None
-
+        ds = h.last_fn._backward(h.ctx, d_output)
+        return zip(h.inputs, ds)
         # TODO: Implement for Task 1.3.
         raise NotImplementedError("Need to implement for Task 1.3")
 
