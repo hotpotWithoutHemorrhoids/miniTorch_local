ea1e5345ba942fbe2cc598985ee401ff0dfe1e29
ysimin <ysimin@infervision.com>
2024-08-15 11:33:04 +0800

    [minitorch]task1_5

--- project/run_scalar.py
+++ project/run_scalar.py
@@ -11,7 +11,10 @@ class Network(minitorch.Module):
     def __init__(self, hidden_layers):
         super().__init__()
         # TODO: Implement for Task 1.5.
-        raise NotImplementedError("Need to implement for Task 1.5")
+        self.layer1 = Linear(2, hidden_layers)
+        self.layer2 = Linear(hidden_layers, hidden_layers)
+        self.layer3 = Linear(hidden_layers, 1)
+        # raise NotImplementedError("Need to implement for Task 1.5")
 
     def forward(self, x):
         middle = [h.relu() for h in self.layer1.forward(x)]
@@ -41,6 +44,12 @@ class Linear(minitorch.Module):
 
     def forward(self, inputs):
         # TODO: Implement for Task 1.5.
+        out = [0]*len(self.bias)
+        for j in range(len(self.bias)):
+            out[j]  = self.bias[j]
+            for i in range(len(inputs)):
+                out[j] += self.weights[i][j]*inputs[i]
+        return out
         raise NotImplementedError("Need to implement for Task 1.5")
 
 
